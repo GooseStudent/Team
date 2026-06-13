b46c8d844e8f46de0253c0152b273be6ea57bad8
Goose test <roganyanegor@yandex.ru>
2026-06-13 18:21:58 +0300

    fix

--- src/Team.js
+++ src/Team.js
@@ -1,24 +1,40 @@
 export class Character {
-  constructor(name, type, health, level, attack, defence) {
-    this.name = name;
-    this.type = type;
-    this.health = health;
-    this.level = level;
-    this.attack = attack;
-    this.defence = defence;
-  }
+    constructor(name, type, health, level, attack, defence) {
+        this.name = name;
+        this.type = type;
+        this.health = health;
+        this.level = level;
+        this.attack = attack;
+        this.defence = defence;
+    }
 }
 
-export class Team { 
+export class Team {
   constructor() {
     this.members = [];
   }
-  
+
   add(character) {
     this.members.push(character);
   }
-  
+
   [Symbol.iterator]() {
-    return this.members[Symbol.iterator]();
+
+    let index = 0;
+    const members = this.members;
+
+    return {
+      next(){
+        if (index < members.length) {
+          return {
+            done: false,
+            value:members[index++]
+          };
+        }
+        return {
+          done: true
+        };
+      }
+    };
   }
 }
